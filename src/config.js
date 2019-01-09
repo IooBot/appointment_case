434ceda5253186ce77b18b9219c2f00443664ee7
kulley <i@szu.im>
2019-01-09 09:31:47 +0800

    change graphql url

--- src/config.js
+++ src/config.js
@@ -1,10 +1,4 @@
 // 这个 fc 的数据结构错了，库存的地方应该是 service_id
-// const graphqlFC = 'http://service-dan6exu1-1254337200.ap-shanghai.apigateway.myqcloud.com/test/graphql';
-
-// const graphqlFC = 'http://localhost:3123/graphql';
-
-// const graphqlFC = 'http://123.206.193.98:3123/graphql';
-const graphqlFC = 'http://order.ioobot.cn/graphql';
-
+const graphqlFC = 'https://order.szu.im/graphql';
 
 export {graphqlFC}
