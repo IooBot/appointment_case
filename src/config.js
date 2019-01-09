3ce82cdf2a607a030905df9ff02828992b1babc9
kulley <i@szu.im>
2019-01-09 15:34:29 +0800

    change graphql api，use cos db file

--- src/config.js
+++ src/config.js
@@ -1,5 +1,3 @@
-// 这个 fc 的数据结构错了，库存的地方应该是 service_id
-// const graphqlFC = 'https://order.szu.im/graphql';
-const graphqlFC = 'https://orderfcdb.ioobot.cn/graphql';
+const graphqlFC = 'https://orderfcdb.szu.im/graphql';
 
 export {graphqlFC}
